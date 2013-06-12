198f4b7866d138ae6424f5aa5dd78e12fe6d0866
Steve Cordle <stevec@allegranetwork.com>
2013-06-12 17:17:09 -0400

    Few edits that were made outside of git and brought in

--- js/gMapper.jquery.js
+++ js/gMapper.jquery.js
@@ -1,11 +1,13 @@
 ;(function($) {
 	$.fn.gMapper = function(options) {
 
+		var instance = 1;
+		
 		var mapTypes = {
-                    "ROADMAP": google.maps.MapTypeId.ROADMAP,
-                    "SATELLITE": google.maps.MapTypeId.SATELLITE,
-                    "HYBRID": google.maps.MapTypeId.HYBRID,
-                    "TERRAIN": google.maps.MapTypeId.TERRAIN
+			"ROADMAP": google.maps.MapTypeId.ROADMAP,
+			"SATELLITE": google.maps.MapTypeId.SATELLITE,
+			"HYBRID": google.maps.MapTypeId.HYBRID,
+			"TERRAIN": google.maps.MapTypeId.TERRAIN
 		};
 			  
 		var defaults = {
@@ -13,18 +15,31 @@
 		  height: 300,
 		  type: "ROADMAP",
 		  zoom: 12,
-                  debug: false
+		  icon: 'http://isite.dev.allegranet.com/Portals/_default/Skins/Allegra_Default/images/map_logo.png'
 		};
 
 		var options = $.extend(true, defaults, options, $(this).data());
 	  
-                var mapOptions = {
-                            zoom: options.zoom,
-                            mapTypeId: mapTypes[options.type]
-                };
-
+	    var mapOptions = {
+			zoom: options.zoom,
+			mapTypeId: mapTypes[options.type]
+	    };
+	  
 		var geocoder = new google.maps.Geocoder();
 		var map = new google.maps.Map($('#'+options.mapid)[0], mapOptions);
+			  
+		var getLatLon = function(address, callback){
+			geocoder.geocode({'address':address},function(results, status){
+				var location = results[0].geometry.location;
+				if(status == google.maps.GeocoderStatus.OK){
+					addresses.push(location);
+				}else{
+					console.log('broken: '+status);
+				}
+				if(callback)
+					callback(location);
+			});
+		}
 		
 		var addMarker = function(data){
 			geocoder.geocode({'address': data.address}, function(results, status){
@@ -32,19 +47,34 @@
 				if(status == google.maps.GeocoderStatus.OK){
 					var marker = new google.maps.Marker({
 						map: map,
-						position: loc
+						position: loc,
+						icon: options.icon
 					});
-					map.setCenter(loc);
+					addToolTip(marker, data.html);
+                    if(data.center === true)
+						map.setCenter(loc);
 				}else{
 					console.log('broken: '+status);
 				}
 			});
 		}
+		
+		var addToolTip = function(marker, message){
+		    var infowindow = new google.maps.InfoWindow({
+		      content: message
+		    });
+
+		    google.maps.event.addListener(marker, 'click', function() {
+		      infowindow.open(marker.get('map'), marker);
+		    });
+		}
       
 		//Perform plugin actions on every matched element
 		this.each(function() {
-			$('#'+options.mapid).css({width:options.width, height:options.height});
+			$('#'+options.mapid).css({width:options.width, height:options.height})
+			           .parent().css({width:options.width, height:options.height});
 			var markers = $(this).children('.marker');
+			markers.hide();
 			markers.each(function(){
 				var data = $(this).data();
 				data.html = $(this).html();
